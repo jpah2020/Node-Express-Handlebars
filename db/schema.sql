fdaed5856fcf81be66ef157a0524ad7a8e77397b
Rusky <jpah.design@gmail.com>
2020-09-22 19:36:06 -0400

    creating js file

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+ DROP DATABASE IF EXISTS burgers_db;
+
+ CREATE DATABASE burgers_db;
+ 
+ use burgers_db;
+
+  CREATE TABLE burgers_tbl(
+    id int primary key auto_increment,
+    burger_name varchar(30) not null,
+    devoured boolean,
+    burger_time timestamp
+  );
--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,4 @@
-insert into burgers_tbl(burger_name) values ('Cheese Burger');
-insert into burgers_tbl(burger_name) values ('Double Quarter With Bacon');
-insert into burgers_tbl(burger_name) values ('Bacon Cheese Burger');
-insert into burgers_tbl(burger_name) values ('Egg Burger');
+insert into burgers_tbl(burger_name, devoured) values ('Cheese Burger');
+insert into burgers_tbl(burger_name, devoured) values ('Double Quarter With Bacon');
+insert into burgers_tbl(burger_name, devoured) values ('Bacon Cheese Burger');
+insert into burgers_tbl(burger_name, devoured) values ('Egg Burger');
