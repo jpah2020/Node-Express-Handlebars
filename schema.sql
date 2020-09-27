52e13271658cc52226a2d27821f8097cc46222dc
Rusky <jpah.design@gmail.com>
2020-09-27 14:41:01 -0400

    make it better

--- schema.sql
+++
@@ -1,8 +0,0 @@
- use burgers_db;
-
-  create table burgers_tbl(
-    id int primary key auto_increment,
-    burger_name varchar(30) not null,
-    devoured boolean,
-    burger_time timestamp
-  );
